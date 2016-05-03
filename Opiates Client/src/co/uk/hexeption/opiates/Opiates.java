fdf3ea9ade83be03a5f660fca227709e0df82f52
Hexeption <minecraftfun201@gmail.com>
2016-05-03 15:27:01 +0100

    Added "Client Name", "Client Version", "Client Creator", "Logger" and "Instance"

---
+++ Opiates Client/src/co/uk/hexeption/opiates/Opiates.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2016 | Hexeption | All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package co.uk.hexeption.opiates;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Opiates {
+
+	public static String	Client_Name		= "Opiates";
+	public static double	Client_Version	= 1.0;
+	public static String	Client_Creator	= "Hexeption";
+	public static Logger	logger			= LogManager.getLogger();
+	public static Opiates	Client_Instance = new Opiates();
+
+	public void startClient() {
+		logger.log(Level.DEBUG, "Loading " + Client_Name);
+		logger.log(Level.DEBUG, "Made by " + Client_Creator);
+
+		// INIT
+
+		logger.log(Level.DEBUG, "Finished loading " + Client_Name);
+	}
+
+	public static String getClient_Name() {
+		return Client_Name;
+	}
+
+	public static double getClient_Version() {
+		return Client_Version;
+	}
+
+	public static String getClient_Creator() {
+		return Client_Creator;
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static Opiates getClient_Instance() {
+		return Client_Instance;
+	}
+	
+	
+
+}
